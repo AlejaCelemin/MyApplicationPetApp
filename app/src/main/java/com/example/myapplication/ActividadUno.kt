3170cbbaebb905190cf348ea0ea587bbb6169943
AlejaCelemin <alejaceleminmartinez@gmail.com>
2021-12-02 20:18:44 -0500

    Logo

---
+++ app/src/main/java/com/example/myapplication/ActividadUno.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ActividadUno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnIntentToactivitytwo.setOnClickListener {
+            startActivity(Intent(this, ActividadDos::class.java))
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
